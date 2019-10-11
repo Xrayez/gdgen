4a2b4f85d578c5869aac51a7460d468fb713619c
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-10-12 00:45:53 +0300

    Refactor module generation by moving into dedicated class

--- init.py
+++ init.py
@@ -5,84 +5,41 @@ import json
 import argparse
 
 import common
-import builders
-from vcs import get_providers as get_vcs_providers
 from messages import *
-from module_wrap import Module
+from module import Module
 
 
 def init(name, output_path, config_path, force=False):
 	root_dir = os.getcwd()
 	
-	# Load config
+	# Load module config
 	try:
 		with open(config_path) as config_data:
 			config = json.load(config_data)
-			module = Module(config)
 	except Exception as e:
 		print_info('config: ' + str(e))
 		return
 		
 	if output_path:
-		module_path = os.path.join(output_path, module.get_short_name())
+		module_path = os.path.join(output_path, config['short_name'])
 	else:
-		module_path = os.path.join(root_dir, module.get_short_name())
-
+		module_path = os.path.join(root_dir, config['short_name'])
+	
+	# Generate module
+	m = Module(config, module_path)
 	try:
 		if os.path.exists(module_path) and force:
-			revert(module_path)
-
-		generate(module, module_path)
+			deinit(module_path)
+		m.generate()
 		
 	except Exception as e:
 		print_info(str(e))
 		print_info(INFO_PASS_FORCE_CMD)
 		
 		
-def revert(path):
+def deinit(path):
 	import shutil
 	shutil.rmtree(path)
-
-
-def generate(module, module_path):
-	
-	# Initialize directory structure
-	os.makedirs(module_path)
-	
-	if module.get_thirdparty_path():
-		thirdparty_dir = os.path.join(module_path, module.get_thirdparty_path())
-		os.makedirs(thirdparty_dir)
-
-	### Generate!
-	
-	# Essential
-	builders.make_config(module, module_path)
-	builders.make_register_types(module, module_path)
-	builders.make_scsub(module, module_path)
-	
-	builders.make_classes(module, module_path)
-	
-	# Optional
-	if module.should_initialize_readme():
-		builders.make_readme(module, module_path)
-		
-	if module.get_license():
-		builders.make_license(module, module_path)
-	
-	if module.get_vcs():
-		initialize_repository(module_path, module.get_vcs())
-		
-		
-def initialize_repository(module_path, vcs_name):
-	
-	for vcs in get_vcs_providers():
-		if not vcs.can_handle(vcs_name):
-			continue
-		vcs.initialize(module_path)
-	
-		
-def open_utf8(filename, mode):
-	return open(filename, mode, encoding="utf-8")
 
 
 def print_info(msg):
--- module.py
+++ module.py
@@ -1,7 +1,44 @@
+import os
+
+import builders
+from vcs import get_providers as get_vcs_providers
+
 
 class Module:
-    def __init__(self, config):
+    def __init__(self, config, module_path):
         self.config = config
+        self.module_path = module_path
+        
+    # Generate methods
+    def generate(self):
+	
+        # Initialize directory structure
+        os.makedirs(self.module_path)
+        
+        if self.get_thirdparty_path():
+            thirdparty_dir = os.path.join(self.module_path, self.get_thirdparty_path())
+            os.makedirs(thirdparty_dir)
+
+        ### Generate!
+        
+        # Essential
+        builders.make_config(self, self.module_path)
+        builders.make_register_types(self, self.module_path)
+        builders.make_scsub(self, self.module_path)
+        
+        builders.make_classes(self, self.module_path)
+        
+        # Optional
+        if self.should_initialize_readme():
+            builders.make_readme(self, self.module_path)
+            
+        if self.get_license():
+            builders.make_license(self, self.module_path)
+        
+        if self.get_vcs():
+            initialize_repository(self.module_path, self.get_vcs())
+        
+    # Config methods
         
     def get_name(self):
         return self.config['name']
@@ -47,3 +84,11 @@ class Module:
         
     def should_include_inside_project(self):
         return self.config['should_include_inside_project']
+
+
+def initialize_repository(module_path, vcs_name):
+	
+	for vcs in get_vcs_providers():
+		if not vcs.can_handle(vcs_name):
+			continue
+		vcs.initialize(module_path)
